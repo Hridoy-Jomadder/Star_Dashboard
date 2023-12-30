4b2479011d3c846a29bd1bd1a4e45487136e4805
Hridoy Jomadder <contacthridoyjomadder.com>
2023-12-30 21:19:44 +0600

    first commit

--- js/main.js
+++ js/main.js
@@ -205,21 +205,3 @@
     
 })(jQuery);
 
-//Todo list js
-function addTask() {
-    const taskInput = document.getElementById("taskInput");
-    const taskList = document.getElementById("taskList");
-
-    if (taskInput.value.trim() !== "") {
-        const li = document.createElement("li");
-        li.innerHTML = `${taskInput.value} <button onclick="removeTask(this)">Delete</button>`;
-        taskList.appendChild(li);
-        taskInput.value = "";
-    }
-}
-
-function removeTask(button) {
-    const li = button.parentElement;
-    const taskList = document.getElementById("taskList");
-    taskList.removeChild(li);
-}
