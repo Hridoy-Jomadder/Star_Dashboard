99ddd9553b67e36e7109d0b5f0781a6089c9fbe1
Hridoy Jomadder <contacthridoyjomadder.com>
2023-12-30 22:11:59 +0600

    first commit

--- signup.php
+++ signup.php
@@ -113,7 +113,7 @@
                             <input value="<?php echo $email ?>" name="email" type="text" id="text" placeholder="Email Address or Phone Number" style="font-family: times new roman;"><br><br>
                             <input value="<?php echo $password ?>" name="password" type="password" id="text" placeholder="Password" style="font-family: times new roman;"><br><br>
                             <input value="<?php echo $password ?>" name="password2" type="password" id="text" placeholder="Retype Password" style="font-family: times new roman;">
-                            <input type="submit" id="button" value="Sign Up" style="font-family: times new roman;">
+                            <input type="submit" id="button" value="Sign Up" style="font-family: times new roman;"><br><br>
                         </form>
 
                         <!-- <div class="form-floating mb-3">
