a4611a0470b2fb2a8672608492a6f8ccd76de4d9
Hridoy Jomadder <contacthridoyjomadder.com>
2023-12-31 18:28:00 +0600

    first commit

--- index.php
+++ index.php
@@ -117,7 +117,7 @@
                         </div>
                     </div>
                     <a href="widget.php" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Widgets</a>
-                    <a href="widget.php" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Widgets</a>
+                    <a href="widget.php" class="nav-item nav-link"><i class="fa fa-envelope me-2"></i>Widgets</a>
                     <a href="form.php" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Forms</a>
                     <a href="table.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Tables</a>
                     <a href="chart.php" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Charts</a>
