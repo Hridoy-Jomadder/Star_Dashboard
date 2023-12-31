a71ff4131fc3e59f35fe4d5f901eaf921bde8978
Hridoy Jomadder <contacthridoyjomadder.com>
2023-12-31 19:39:24 +0600

    first commit

--- index.php
+++ index.php
@@ -247,7 +247,7 @@
                     </div>
                     <div class="col-sm-6 col-xl-3">
                         <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
-                            <i class="fa fa-chart-bar fa-3x text-primary"></i>
+                            <i class="fa fa-chart-area fa-3x text-primary"></i>
                             <div class="ms-3">
                                 <p class="mb-2">Total Star</p>
                                 <h6 class="mb-0">123</h6>
@@ -265,7 +265,7 @@
                     </div>
                     <div class="col-sm-6 col-xl-3">
                         <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
-                            <i class="fa fa-chart-area fa-3x text-primary"></i>
+                            <i class="fa fa-chart-bar fa-3x text-primary"></i>
                             <div class="ms-3">
                                 <p class="mb-2">All Star ID</p>
                                 <h6 class="mb-0">1234</h6>
