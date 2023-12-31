5843d151a99e24b3dc94dcccf5550d144037c465
Hridoy Jomadder <contacthridoyjomadder.com>
2023-12-31 18:21:37 +0600

    first commit

--- index.php
+++ index.php
@@ -257,7 +257,7 @@
                         <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
                             <i class="fa fa-chart-area fa-3x text-primary"></i>
                             <div class="ms-3">
-                                <p class="mb-2">All Star</p>
+                                <p class="mb-2">All Star ID</p>
                                 <h6 class="mb-0">1234</h6>
                             </div>
                         </div>
