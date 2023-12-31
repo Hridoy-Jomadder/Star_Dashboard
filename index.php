b15ed300bc511dc60f619a2805d13fe8e1cf41f2
Hridoy Jomadder <contacthridoyjomadder.com>
2023-12-31 18:58:56 +0600

    first commit

--- index.php
+++ index.php
@@ -111,9 +111,9 @@
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-laptop me-2"></i>Star Dev</a>
                         <div class="dropdown-menu bg-transparent border-0">
-                            <a href="button.php" class="dropdown-item">Buttons</a>
-                            <a href="typography.php" class="dropdown-item">Typography</a>
-                            <a href="element.php" class="dropdown-item">Other Elements</a>
+                            <a href="button.php" class="dropdown-item">CEO</a>
+                            <a href="typography.php" class="dropdown-item">Co-CEO</a>
+                            <a href="element.php" class="dropdown-item">Star Member</a>
                         </div>
                     </div>
                     <a href="message.php" class="nav-item nav-link"><i class="fa fa-envelope me-2"></i>Message</a>
