7aebdfe095efc0d1ccfee27f53db7008d6ea2c78
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-01 06:40:04 +0600

    update

--- index.php
+++ index.php
@@ -141,7 +141,7 @@
             <!-- Navbar Start -->
             <nav class="navbar navbar-expand bg-light navbar-light sticky-top px-4 py-0">
                 <a href="index.php" class="navbar-brand d-flex d-lg-none me-4">
-                    <h2 class="text-primary mb-0"><i class="fa fa-hashtag"></i></h2>
+                    <h2 class="text-primary mb-0"><i class="fa fa-star"></i></h2>
                 </a>
                 <a href="#" class="sidebar-toggler flex-shrink-0">
                     <i class="fa fa-bars"></i>
