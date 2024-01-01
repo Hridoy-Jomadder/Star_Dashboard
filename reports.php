69f302419c8e390bc7266f718fc5e8ce8892148a
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-01 20:40:26 +0600

    update

--- reports.php
+++ reports.php
@@ -183,7 +183,6 @@
                 <div class="bg-light text-center rounded p-4">
                     <div class="d-flex align-items-center justify-content-between mb-4">
                         <h6 class="mb-0">Star Account</h6>
-                        <a href="">Show All</a>
                     </div>
                     <div class="table-responsive">
                         <table class="table text-start align-middle table-bordered table-hover mb-0">
