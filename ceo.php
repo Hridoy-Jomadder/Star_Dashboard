9705c3604c72510a7d15aaab371e340f66366d6e
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-01 20:56:54 +0600

    update

--- ceo.php
+++ ceo.php
@@ -188,7 +188,7 @@
                         <div class="bg-light rounded h-100 p-4">
                             <h6 class="mb-4">File Input</h6>
                             <div class="mb-3">
-                                <label for="formFile" class="form-label">Default file input example</label>
+                                <!-- <label for="formFile" class="form-label">Default file input example</label> -->
                                 <input class="form-control" type="file" id="formFile">
                             </div>
                         </div>
