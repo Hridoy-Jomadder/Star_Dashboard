10c9f635519aab55a44f386feb1802e9fde7bcbe
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-01 20:09:47 +0600

    update

--- worldwide.php
+++ worldwide.php
@@ -41,7 +41,7 @@
         </div>
         <!-- Spinner End -->
 
-
+<!--
         <!-- Sidebar Start -->
         <div class="sidebar pe-4 pb-3">
             <nav class="navbar bg-light navbar-light">
@@ -464,7 +464,7 @@
             <!-- Footer End -->
         </div>
         <!-- Content End -->
-
+        -->
 
         <!-- Back to Top -->
         <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
