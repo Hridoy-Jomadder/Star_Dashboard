1e93134b8e654a767b901fa1a487db10500ce407
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-02 20:56:32 +0600

    first commit

--- ceo.php
+++ ceo.php
@@ -258,12 +258,12 @@ function getprofile_image($userId) {
                     <div class="col-md-6 text-center">
                     <div class="col-sm-12 col-xl-6">
                         <div class="bg-light rounded h-100 p-4">
-                            <img src="img/photo.png" style="height:300px;width: 300px;">
+                            <img src="
                             <?php
                             // Display the user's profile photo
                             echo '<img src="uploads/' . $profile_image . '" alt="Profile Photo" width="150" height="150">';
                             ?>
-
+                             " style="height:300px;width: 300px;">
                             <h5 class="mb-0"><?php echo $user_data['first_name'] . " " . $user_data['last_name']?></h6>
                             <h6 class="mb-4"><?php echo $user_data['title'] ?></h5>
                             <div class="mb-3">
