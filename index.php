f4d30b847678e9e84dcbc517d859f67c5e34e310
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-02 22:20:54 +0600

    update

--- index.php
+++ index.php
@@ -290,7 +290,7 @@ function getprofile_image($userId) {
                 <div class="bg-light text-center rounded p-4">
                     <div class="d-flex align-items-center justify-content-between mb-4">
                         <h6 class="mb-0">Star Account</h6>
-                        <a href="">Show All</a>
+                        <a href="reports.php">Show All</a>
                     </div>
                     <div class="table-responsive">
                         <table class="table text-start align-middle table-bordered table-hover mb-0">
