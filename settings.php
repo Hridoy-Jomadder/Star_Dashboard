3b10b44584238333dc67fc9d50be9864ac3edfcb
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-02 22:54:05 +0600

    update

--- settings.php
+++ settings.php
@@ -5,6 +5,7 @@
  include("classes/connect.php");
  include("classes/login.php");
  include("classes/user.php");
+ include("classes/settings.php");
 
  //check if user is logged in
  if(isset($_SESSION['das_userid']) && is_numeric($_SESSION['das_userid']))
