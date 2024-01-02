c8cddb7b323e56db3e88bc56ca80b78cbdf04eb0
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-02 21:41:13 +0600

    update

--- index.php
+++ index.php
@@ -119,9 +119,7 @@ function getprofile_image($userId) {
                 </a>
                 <div class="d-flex align-items-center ms-4 mb-4">
                     <div class="position-relative">
-                    <?php
-                            echo '<img src="uploads/' . $profile_image . '" width="50px" height="50px" class="rounded-circle">';
-                            ?>
+                    <?php echo '<img src="uploads/' . $profile_image . '" width="50px" height="50px" class="rounded-circle">'; ?>
                         <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
                     </div>
                     <div class="ms-3">
@@ -173,10 +171,7 @@ function getprofile_image($userId) {
                         <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
                             <a href="#" class="dropdown-item">
                                 <div class="d-flex align-items-center">
-                                <?php
-                                    echo '<img src="uploads/' . $profile_image . '" width="30px" height="30px">';
-                                    ?>
-                                    <!-- <img class="rounded-circle" src="img/user.jpg" alt="" style="width: 40px; height: 40px;"> -->
+                        
                                     <div class="ms-2">
                                         <h6 class="fw-normal mb-0">Jhon send you a message</h6>
                                         <small>15 minutes ago</small>
@@ -233,9 +228,7 @@ function getprofile_image($userId) {
                     </div>
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                        <?php
-                            echo '<img src="uploads/' . $profile_image . '" width="30px" height="30px">';
-                            ?>
+                        <?php echo '<img src="uploads/' . $profile_image . '" width="50px" height="50px" class="rounded-circle">'; ?>
                             <span class="d-none d-lg-inline-flex"><?php echo $user_data['first_name'] . " " . $user_data['last_name']?></span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
