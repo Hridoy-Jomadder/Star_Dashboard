818702b880abb0f86a47de84b31f34a229604507
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-02 21:31:02 +0600

    update

--- index.php
+++ index.php
@@ -171,9 +171,8 @@ function getprofile_image($userId) {
                             <a href="#" class="dropdown-item">
                                 <div class="d-flex align-items-center">
                                 <?php
-                            // Display the user's profile photo
-                            echo '<img src="uploads/' . $profile_image . '" alt="Profile Photo" width="30" height="30">';
-                            ?>
+                                    echo '<img src="uploads/' . $profile_image . '" alt="Profile Photo" width="30" height="30">';
+                                    ?>
                                     <!-- <img class="rounded-circle" src="img/user.jpg" alt="" style="width: 40px; height: 40px;"> -->
                                     <div class="ms-2">
                                         <h6 class="fw-normal mb-0">Jhon send you a message</h6>
