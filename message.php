1b8e570b0d50fa2f8bd369dbeddf1d4143410cad
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-03 16:37:33 +0600

    update

--- message.php
+++ message.php
@@ -243,9 +243,9 @@ function getprofile_image($userId) {
 
  
                      <!-- Chart Start -->
-            <div class="container-fluid pt-4 px-4">
-                <div class="row g-12">
-                    <div class="col-sm-12 col-md-6 col-xl-4">
+                     <div class="container-fluid pt-4 px-4">
+                <div class="bg-light text-center rounded p-4">
+                    <div class="d-flex align-items-center justify-content-between mb-4">
                         <div class="h-100 bg-light rounded p-4">
                             <div class="d-flex align-items-center justify-content-between mb-2">
                                 <h6 class="mb-0">Messages</h6>
