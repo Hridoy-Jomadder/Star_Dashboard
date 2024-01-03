ca3db6b685f23c7b33e3068bcad6bd4dbd77c45f
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-03 22:05:39 +0600

    update

--- classes/login.php
+++ classes/login.php
@@ -1,81 +1,61 @@
-<?php
-
 class Login
 {
 
-	private $error = "";
+    private $error = "";
  
-	public function evaluate($data)
-	{
-
-		$email = addslashes($data['email']);
-		$password = addslashes($data['password']);
-
-		$query = "select * from users where email = '$email' limit 1 ";
-
-		$DB = new Database();
-		$result = $DB->read($query);
-  
-		if($result)
-		{
-
-			$row = $result[0];
-
-			if($password == $row['password'])
-			{
-
-				//create session data
-				$_SESSION['das_userid'] = $row['userid'];
-
-			}else
-			{
-				$this->error .= "wrong email or password<br>";
-			}
-		}else
-		{
-
-			$this->error .= "No Such email was found.<br>";
-		}
-
-		return $this->error;
-		
-	}
-    
-    public function check_login($id,$redirect = true)
+    public function evaluate($data)
     {
-        if (is_numeric($id))
-           {
-                $query = "select * from users where userid = '$id' limit 1 ";
+        $email = addslashes($data['email']);
+        $password = addslashes($data['password']);
 
-                $DB = new Database();
-                $result = $DB->read($query);
+        $query = "SELECT * FROM users WHERE email = '$email' LIMIT 1 ";
 
-                // Add this code in the check_login function after verifying credentials
-                if ($result) {
-                  $_SESSION['das_user_role'] = $user_data['role'];
+        $DB = new Database();
+        $result = $DB->read($query);
+  
+        if ($result) {
+            $row = $result[0];
+
+            if ($password == $row['password']) {
+                // Create session data
+                $_SESSION['das_userid'] = $row['userid'];
+                $_SESSION['das_user_role'] = $row['role'];
+            } else {
+                $this->error .= "Wrong email or password<br>";
+            }
+        } else {
+            $this->error .= "No such email was found.<br>";
+        }
+
+        return $this->error;
+    }
+    
+    public function check_login($id, $redirect = true)
+    {
+        if (is_numeric($id)) {
+            $query = "SELECT * FROM users WHERE userid = '$id' LIMIT 1 ";
+
+            $DB = new Database();
+            $result = $DB->read($query);
+
+            if ($result) {
+                $user_data = $result[0];
+                return $user_data;
+            } else {
+                if ($redirect) {
+                    header("Location: login.php");
+                    die;
+                } else {
+                    $_SESSION['das_userid'] = 0;
                 }
-                else
-                {
-                 if($redirect){
-                   header("Location: login.php");
-                   die;
-                 }else{
-                     
-                  $_SESSION['das_userid'] = 0;
-                     
-                 }
-               }   
-         }else
-         {
-
-         if($redirect){
+            }
+        } else {
+            if ($redirect) {
                 header("Location: login.php");
                 die;
-                 }else{
-             
-                 $_SESSION['das_userid'] = 0;
-             
-                 }
+            } else {
+                $_SESSION['das_userid'] = 0;
+            }
         }
-   }
+    }
 }
