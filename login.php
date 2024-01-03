6770c16f2680104e1539e624b30c5c5b0eed6238
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-03 11:13:06 +0600

    update

--- login.php
+++ login.php
@@ -42,10 +42,11 @@ session_start();
 
 <head>
     <meta charset="utf-8">
-    <title>DASHMIN - Bootstrap Admin Template</title>
+    <title>DASHMIN - Star</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
+    <meta content="Hridoy Jomadder"name="auther">
 
     <!-- Favicon -->
     <link href="img/favicon.ico" rel="icon">
