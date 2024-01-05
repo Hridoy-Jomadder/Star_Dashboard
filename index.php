bb55abf732285a7abac5d39fc057b57bd402727a
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-05 20:37:43 +0600

    update

--- index.php
+++ index.php
@@ -93,11 +93,17 @@ if (isset($_SESSION['das_userid']) && is_numeric($_SESSION['das_userid'])) {
                 </a>
                 <div class="d-flex align-items-center ms-4 mb-4">
                     <div class="position-relative">
-                    <?php // Assuming you have retrieved the user's profile image path in your user data
-echo $profile_image = $user_data['profile_image'];
+                    <?php // Assuming $user_data contains the user's data including the profile image path
+if(isset($user_data['profile_image'])){
+    $profile_image = $user_data['profile_image'];
+} else {
+    // Set a default image or handle the case where profile_image is not set
+    $profile_image = 'default_profile_image.jpg';
+}
 
 // Now you can use $profile_image in your HTML code
-//  '<img src="uploads/' . $profile_image . '" width="50px" height="50px" class="rounded-circle">';
+echo '<img src="uploads/' . $profile_image . '" width="50px" height="50px" class="rounded-circle">';
+
  ?>
                         <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
                     </div>
