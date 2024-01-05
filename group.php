fae1fd213be081834749e81f302ea660e01bc99e
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-05 20:03:25 +0600

    update

--- group.php
+++ group.php
@@ -250,11 +250,11 @@ function getprofile_image($userId) {
                             <div class="d-flex align-items-center justify-content-between mb-2">
                                 <h6 class="mb-0">CEO</h6>
                             </div>
-                            <div class="d-flex align-items-center border-bottom py-3">
+                            <div class="d-flex align-items-center border-top py-3">
                                 <img class="rounded-circle flex-shrink-0" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
                                 <div class="w-100 ms-3">
                                     <div class="d-flex w-100 justify-content-between">
-                                        <h6 class="mb-0" class="text-primary">Jhon Doe</h6>
+                                        <h6 class="mb-0">Jhon Doe</h6>
                                         <small>15 minutes ago</small>
                                     </div>
                                     <span>Short message goes here...</span>
