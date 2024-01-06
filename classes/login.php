f8e6240812ca612c20cbcc77849f491888f4a73e
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-06 16:39:06 +0600

    update

--- classes/login.php
+++ classes/login.php
@@ -1,23 +1,24 @@
 <?php
+
 class Login
 {
-
     private $error = "";
- 
+
     public function evaluate($data)
     {
-        $email = addslashes($data['email']);
-        $password = addslashes($data['password']);
-
-        $query = "SELECT * FROM users WHERE email = '$email' LIMIT 1 ";
-
         $DB = new Database();
+
+        // Sanitize input
+        $email = $DB->escape_string($data['email']);
+        $password = $DB->escape_string($data['password']);
+
+        $query = "SELECT * FROM users WHERE email = '$email' LIMIT 1";
         $result = $DB->read($query);
-  
+
         if ($result) {
             $row = $result[0];
 
-            if ($password == $row['password']) {
+            if (password_verify($password, $row['password'])) {
                 // Create session data
                 $_SESSION['das_userid'] = $row['userid'];
                 $_SESSION['das_user_role'] = $row['role'];
@@ -30,13 +31,13 @@ class Login
 
         return $this->error;
     }
-    
+
     public function check_login($id, $redirect = true)
     {
-        if (is_numeric($id)) {
-            $query = "SELECT * FROM users WHERE userid = '$id' LIMIT 1 ";
+        $DB = new Database();
 
-            $DB = new Database();
+        if (is_numeric($id)) {
+            $query = "SELECT * FROM users WHERE userid = '$id' LIMIT 1";
             $result = $DB->read($query);
 
             if ($result) {
@@ -45,18 +46,16 @@ class Login
             } else {
                 if ($redirect) {
                     header("Location: login.php");
-                    die;
-                } else {
-                    $_SESSION['das_userid'] = 0;
                 }
             }
         } else {
             if ($redirect) {
                 header("Location: login.php");
-                die;
-            } else {
-                $_SESSION['das_userid'] = 0;
             }
         }
+
+        $_SESSION['das_userid'] = 0;
     }
 }
+
+?>
