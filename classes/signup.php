3429cd1014375fad6bc45526c7790ce32b0ab4a9
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-06 17:17:36 +0600

    update

--- classes/signup.php
+++ classes/signup.php
@@ -9,33 +9,28 @@ class Signup
         include_once("classes/database.php");
     }
 
-    public function registerUser($data)
+    public function evaluate($data)
     {
         $DB = new Database();
 
         // Sanitize input
-        $email = $DB->escape_string($data['email']);
+        $username = $DB->escape_string($data['username']);
         $password = $DB->escape_string($data['password']);
-        // You may want to add additional validations and checks
+        $email = $DB->escape_string($data['email']);
 
-        // Check if the email already exists
-        $existingUser = $DB->readWithParams("SELECT * FROM users WHERE email = ?", [$email]);
+        // Use prepared statements
+        $query = "INSERT INTO users (username, password, email, role) VALUES (?, ?, ?, 'StarMember')";
+        $params = [$username, password_hash($password, PASSWORD_DEFAULT), $email];
 
-        if ($existingUser) {
-            $this->error .= "Email already exists. Please choose a different one.<br>";
+        $result = $DB->execute($query, $params);
+
+        if ($result) {
+            // Registration successful
+            return true;
+        } else {
+            $this->error = "Error registering user";
             return $this->error;
         }
-
-        // Insert the new user into the database
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-        $insertQuery = "INSERT INTO users (email, password) VALUES (?, ?)";
-        $result = $DB->write($insertQuery, [$email, $hashedPassword]);
-
-        if (!$result) {
-            $this->error .= "Registration failed. Please try again.<br>";
-        }
-
-        return $this->error;
     }
 }
 
