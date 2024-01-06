a8591995ed7dafcc48704b9b729256129514908e
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-06 16:37:05 +0600

    update

--- classes/user.php
+++ classes/user.php
@@ -1,35 +1,29 @@
 <?php 
-
 class User
 {
+    public function get_user_by_id($id)
+    {
+        $query = "SELECT * FROM users WHERE userid = ? LIMIT 1";
+        $DB = new Database();
+        $result = $DB->readWithParams($query, [$id]);
 
-	public function get_user($id)
-	{
-		$query = "SELECT * FROM users WHERE userid = ? LIMIT 1";
-		$DB = new Database();
-		$result = $DB->readWithParams($query, [$id]);
-	
-		if ($result) {
-			return $result[0];
-		} else {
-			return false;
-		}
-	}
-	
-    public function get_user($id)
-	{
+        if ($result) {
+            return $result[0];
+        } else {
+            return false;
+        }
+    }
 
-		$query = "select * from users where userid = '$id' limit 1";
-		$DB = new Database();
-		$result = $DB->read($query);
+    public function get_user_by_username($username)
+    {
+        $query = "SELECT * FROM users WHERE username = ? LIMIT 1";
+        $DB = new Database();
+        $result = $DB->readWithParams($query, [$username]);
 
-		if($result)
-		{
-			return $result[0];
-		}else
-		{
-
-			return false;
-		}
-	}
+        if ($result) {
+            return $result[0];
+        } else {
+            return false;
+        }
+    }
 }
