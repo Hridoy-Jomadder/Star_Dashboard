3ebe648c364c135fc732e2e0507cdaa05b304c7a
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-06 17:06:46 +0600

    update

--- classes/login.php
+++ classes/login.php
@@ -10,40 +10,40 @@ class Login
     }
 
     public function evaluate($data)
-{
-    $DB = new Database();
-
-    // Check if 'email' key exists in $data
-    if (!isset($data['email'])) {
-        $this->error .= "Email is required.<br>";
+    {
+        $DB = new Database();
+    
+        // Check if 'email' key is set in $_POST
+        if (isset($data['email'])) {
+            // Sanitize input
+            $email = $DB->escape_string($data['email']);
+            $password = $DB->escape_string($data['password']);
+        
+            // Use prepared statements
+            $query = "SELECT * FROM users WHERE email = ? LIMIT 1";
+            $result = $DB->readWithParams($query, [$email]);
+        
+            if ($result) {
+                $row = $result[0];
+        
+                if (password_verify($password, $row['password'])) {
+                    // Create session data
+                    $_SESSION['das_userid'] = $row['userid'];
+                    $_SESSION['das_user_role'] = $row['role'];
+                } else {
+                    $this->error .= "Wrong email or password<br>";
+                }
+            } else {
+                $this->error .= "No such email was found.<br>";
+            }
+        } else {
+            $this->error .= "Email not provided.<br>";
+        }
+    
         return $this->error;
     }
 
-    // Sanitize input
-    $email = $DB->escape_string($data['email']);
-    $password = $DB->escape_string($data['password']);
-
-    // Use prepared statements
-    $query = "SELECT * FROM users WHERE email = ? LIMIT 1";
-    $result = $DB->readWithParams($query, [$email]);
-
-    if ($result) {
-        $row = $result[0];
-
-        if (password_verify($password, $row['password'])) {
-            // Create session data
-            $_SESSION['das_userid'] = $row['userid'];
-            $_SESSION['das_user_role'] = $row['role'];
-        } else {
-            $this->error .= "Wrong email or password<br>";
-        }
-    } else {
-        $this->error .= "No such email was found.<br>";
-    }
-
-    return $this->error;
+    // ... rest of your class ...
 }
-}
-
 
 ?>
