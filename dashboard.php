4da0d0b38fc4ecf986f498d9494c91337956318d
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-07 18:33:24 +0600

    update

--- dashboard.php
+++ dashboard.php
@@ -214,8 +214,6 @@ $role = $user['role'];
             </nav>
             <!-- Navbar End -->
 
-
-
      <!-- Display CEO's profile information -->
      <div class="container-fluid pt-4 px-4">
                 <div class="row vh-100 bg-light rounded align-items-center justify-content-center mx-0">
