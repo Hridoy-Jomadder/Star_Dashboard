a84ad528d1b5a259e7dd3b90184e97e63ac1a95e
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-08 19:15:49 +0600

    update

--- message.php
+++ message.php
@@ -122,7 +122,7 @@ session_start();
                         <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
                     </div>
                     <div class="ms-3">
-                        <h6 class="mb-0"><?php echo $first_name . ' ' . $last_name; ?></h6>
+                    <h6 class="mb-0"><?php echo $first_name . ' ' . $last_name; ?></h6>
                         <small><?php echo $role; ?></small>
                     </div>
                 </div>
