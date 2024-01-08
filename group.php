c58957f5ee56f995226069ce4911901aa3a55496
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-08 18:31:14 +0600

    update

--- group.php
+++ group.php
@@ -55,8 +55,8 @@
                         <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
                     </div>
                     <div class="ms-3">
-                        <h6 class="mb-0"><?php echo $user_data['first_name'] . " " . $user_data['last_name']?></h6>
-                        <small><?php echo $user_data['title'] ?></small>
+                    <h6 class="mb-0"><?php echo $first_name . ' ' . $last_name; ?></h6>
+                        <small><?php echo $role; ?></small>
                     </div>
                 </div>
                 <div class="navbar-nav w-100">
