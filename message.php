85c587ba6d16675cbe3f48f1b970ac41f42aa334
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-08 18:27:45 +0600

    update

--- message.php
+++ message.php
@@ -118,7 +118,7 @@ session_start();
                 </a>
                 <div class="d-flex align-items-center ms-4 mb-4">
                     <div class="position-relative">
-                    <?php echo '<img src="uploads/' . $profile_image . '" width="50px" height="50px" class="rounded-circle">'; ?>
+                    <!-- <?php echo '<img src="uploads/' . $profile_image . '" width="50px" height="50px" class="rounded-circle">'; ?> -->
                         <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
                     </div>
                     <div class="ms-3">
