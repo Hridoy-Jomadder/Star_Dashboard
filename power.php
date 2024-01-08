e404fec6f420ed3f6cb919133f39bad31f09178e
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-08 18:55:38 +0600

    update

--- power.php
+++ power.php
@@ -107,8 +107,8 @@ if ($user['role'] === 'star_member') {
                         <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
                     </div>
                     <div class="ms-3">
-                        <h6 class="mb-0"><?php echo $user_data['first_name'] . " " . $user_data['last_name']?></h6>
-                        <small><?php echo $user_data['title'] ?></small>
+                    <h6 class="mb-0"><?php echo $first_name . ' ' . $last_name; ?></h6>
+                        <small><?php echo $role; ?></small>
                     </div>
                 </div>
                 <div class="navbar-nav w-100">
@@ -213,7 +213,7 @@ if ($user['role'] === 'star_member') {
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                         <?php echo '<img src="uploads/' . $profile_image . '" width="40px" height="40px" class="rounded-circle">'; ?>
-                            <span class="d-none d-lg-inline-flex"><?php echo $user_data['first_name'] . " " . $user_data['last_name']?></span>
+                            <span class="d-none d-lg-inline-flex"><?php echo $first_name . ' ' . $last_name; ?></span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
                             <a href="ceo.php" class="dropdown-item">My Profile</a>
