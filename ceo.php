5a48d3cd6a03c18f40d5411c610fc79b041b47e1
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-08 21:01:06 +0600

    update

--- ceo.php
+++ ceo.php
@@ -113,7 +113,7 @@ session_start();
         <!-- Sidebar Start -->
         <div class="sidebar pe-4 pb-3">
             <nav class="navbar bg-light navbar-light">
-                <a href="index.php" class="navbar-brand mx-4 mb-3">
+                <a href="" class="navbar-brand mx-4 mb-3">
                     <h3 class="text-primary"><i class="fa fa-star me-2"></i>DASHMIN</h3>
                 </a>
                 <div class="d-flex align-items-center ms-4 mb-4">
@@ -152,7 +152,7 @@ session_start();
         <div class="content">
             <!-- Navbar Start -->
             <nav class="navbar navbar-expand bg-light navbar-light sticky-top px-4 py-0">
-                <a href="index.php" class="navbar-brand d-flex d-lg-none me-4">
+                <a href="" class="navbar-brand d-flex d-lg-none me-4">
                     <h2 class="text-primary mb-0"><i class="fa fa-star"></i></h2>
                 </a>
                 <a href="#" class="sidebar-toggler flex-shrink-0">
