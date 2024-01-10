1ff928bb936ac0fb32a9fe0a85ecadf17bfd629a
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-10 21:38:57 +0600

    update

--- dashboard.php
+++ dashboard.php
@@ -591,8 +591,8 @@ session_start();
 <script>
     document.addEventListener('DOMContentLoaded', function () {
         // Sample data
-        var countries = ['BD', 'IND', 'PAK'];
-        var data = [150, 200, 100];
+        var countries = ['BD', 'IND', 'PAK','AFG'];
+        var data = [150, 200, 100,50];
 
         // Get the canvas element
         var ctxWorldwideCountry = document.getElementById('worldwide-country').getContext('2d');
@@ -609,11 +609,13 @@ session_start();
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
                         'rgba(255, 206, 86, 0.2)',
+                        'rgba(255, 106, 186, 0.2)',
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
                         'rgba(54, 162, 235, 1)',
                         'rgba(255, 206, 86, 1)',
+                        'rgba(255, 106, 186, 1)',
                     ],
                     borderWidth: 1
                 }]
