dea4f6d6f6990c400e8549093bbcda3d0d0874c6
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-10 20:02:56 +0600

    update

--- message.php
+++ message.php
@@ -231,7 +231,7 @@ if ($user['role'] === 'star_member') {
                 <div class="row g-12">
                     <div class="col-sm-12 col-md-6 col-xl-12">
                         <div class="h-100 bg-light rounded p-4">
-                            <div class="d-flex align-items-right justify-content-between mb-2">
+                            <div class="d-flex align-items-center justify-content-between mb-2">
                                 <h6 class="mb-0">Messages</h6>
                             </div>
                             <div class="d-flex align-items-center border-bottom py-3">
@@ -248,8 +248,8 @@ if ($user['role'] === 'star_member') {
                                 <img class="rounded-circle flex-shrink-0" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
                                 <div class="w-100 ms-3">
                                     <div class="d-flex w-100 justify-content-between">
-                                        <h6 class="mb-0">Jhon Doe</h6>
                                         <small>15 minutes ago</small>
+                                        <h6 class="mb-0">Jhon Doe</h6>
                                     </div>
                                     <span>Short message goes here...</span>
                                 </div>
