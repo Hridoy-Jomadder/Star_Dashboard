83728ccdfe28bf0bbc75c4d407be58b7b0c0a886
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-10 19:57:39 +0600

    update

--- message.php
+++ message.php
@@ -237,7 +237,7 @@ if ($user['role'] === 'star_member') {
                             <div class="d-flex align-items-center border-bottom py-3">
                                 <img class="rounded-circle flex-shrink-0" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
                                 <div class="w-100 ms-3">
-                                    <div class="d-flex w-100 justify-content-between">
+                                    <div class="d-flex w-100">
                                         <h6 class="mb-0" style="text-align: right;">Jhon Doe</h6>
                                         <small>15 minutes ago</small>
                                     </div>
