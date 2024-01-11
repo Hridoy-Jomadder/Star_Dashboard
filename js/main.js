32631cb03599c90c89dc41c7c1e71a5816f4d201
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-11 20:30:35 +0600

    update

--- js/main.js
+++ js/main.js
@@ -163,14 +163,15 @@
     var myChart5 = new Chart(ctx5, {
         type: "pie",
         data: {
-            labels: ["Italy", "France", "Spain", "USA", "Argentina"],
+            labels: ["Italy", "France", "Spain", "USA", "Argentina", "Bangladesh"],
             datasets: [{
                 backgroundColor: [
                     "rgba(0, 156, 255, .7)",
                     "rgba(0, 156, 255, .6)",
                     "rgba(0, 156, 255, .5)",
                     "rgba(0, 156, 255, .4)",
-                    "rgba(0, 156, 255, .3)"
+                    "rgba(0, 156, 255, .3)",
+                    "rgba(255, 156, 255, .3)"
                 ],
                 data: [55, 49, 44, 24, 15]
             }]
