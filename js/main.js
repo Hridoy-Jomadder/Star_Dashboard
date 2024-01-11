b36fba95a4556ed8790ec6f09c80464064bd8d8c
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-11 20:15:59 +0600

    update

--- js/main.js
+++ js/main.js
@@ -120,12 +120,12 @@
     var myChart3 = new Chart(ctx3, {
         type: "line",
         data: {
-            labels: [55, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
+            labels: [0, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
             datasets: [{
                 label: "Salse",
                 fill: true,
                 backgroundColor: "rgba(0, 156, 255, .3)",
-                data: [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15]
+                data: [0, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15]
             }]
         },
         options: {
