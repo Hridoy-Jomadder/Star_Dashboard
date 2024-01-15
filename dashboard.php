86d34544a1be15fd133cdd48fa49a762b597d5ad
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-15 23:47:31 +0600

    update

--- dashboard.php
+++ dashboard.php
@@ -405,28 +405,6 @@ session_start();
                             <div id="calender"></div>
                         </div>
                     </div>
-
-
-                    <div class="col-sm-12 col-md-6 col-xl-4">
-                        <div class="h-100 bg-light rounded p-4">
-                            <div class="d-flex align-items-center justify-content-between mb-4">
-                                <h6 class="mb-0">To Do List</h6>
-                            </div>
-                            <div class="d-flex mb-2">
-                                <input class="form-control bg-transparent" type="text" id="taskInput" placeholder="Enter task">
-                                <button type="button" class="btn btn-primary ms-2" onclick="addTask()">Add Task</button>
-                            </div>
-                            <div class="d-flex align-items-center border-bottom py-2">
-                                <!-- <input class="form-check-input m-0" type="checkbox"> -->
-                                <div class="w-100 ms-3">
-                                    <div class="d-flex w-100 align-items-center justify-content-between">
-                                        <div id="taskList"></div>
-                                        <!-- <button class="btn btn-sm"><i class="fa fa-times"></i></button> -->
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
             <!-- Widgets End -->
