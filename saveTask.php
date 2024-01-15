0b36ffa63fda1241c138d3e2352b978f2f0e99f4
Hridoy Jomadder <contacthridoyjomadder.com>
2024-01-15 22:19:51 +0600

    update

---
+++ saveTask.php
@@ -0,0 +1,34 @@
+<?php
+if (isset($_GET['taskName'])) {
+    // Get the task name from the query parameter
+    $taskName = $_GET['taskName'];
+
+    // Establish a connection to the database
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "das_db";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check the connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Insert the task into the tasks table
+    $sql = "INSERT INTO tasks (task_name) VALUES ('$taskName')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Task saved successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    // Close the connection
+    $conn->close();
+} else {
+    // Handle the case where "taskName" is not set in the $_GET array
+    echo "Error: 'taskName' not set in the query parameters.";
+}
+?>
