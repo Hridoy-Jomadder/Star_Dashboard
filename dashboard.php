a0abbdb32dad80295c3204de41e77e7038e5efbf
Hridoy Jomadder <contacthridoyjomadder.com>
2024-04-14 12:48:04 +0600

    update

--- dashboard.php
+++ dashboard.php
@@ -254,7 +254,7 @@ session_start();
                         <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
                             <i class="fa fa-chart-area fa-3x text-primary"></i>
                             <div class="ms-3">
-                                <p class="mb-2">Total Star</p>
+                                <p class="mb-2">Total Online</p>
                                 <h6 class="mb-0">123</h6>
                             </div>
                         </div>
@@ -276,7 +276,8 @@ session_start();
                                 <h6 class="mb-0">1234</h6>
                             </div>
                         </div>
-                    </div>                    <div class="col-sm-6 col-xl-3">
+                    </div> 
+                   <div class="col-sm-6 col-xl-3">
                         <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
                             <i class="fa fa-male fa-3x text-primary"></i>
                             <div class="ms-3">
